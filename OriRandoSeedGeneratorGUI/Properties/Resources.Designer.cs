19eb6a10a32adc41948909a322e9b1550bfc6176
David C. Miller <david.cornelius.miller@gmail.com>
2017-05-25 20:44:59 -0400

    Modfications to publish settings

--- OriRandoSeedGeneratorGUI/Properties/Resources.Designer.cs
+++ OriRandoSeedGeneratorGUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OriRandoSeedGeneratorGUI.Properties {
+namespace OriDERandoSeedGeneratorGUI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace OriRandoSeedGeneratorGUI.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OriRandoSeedGeneratorGUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OriDERandoSeedGeneratorGUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- OriRandoSeedGeneratorGUI/Properties/Settings.Designer.cs
+++ OriRandoSeedGeneratorGUI/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OriRandoSeedGeneratorGUI.Properties {
+namespace OriDERandoSeedGeneratorGUI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
